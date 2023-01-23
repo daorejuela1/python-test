d1b168365bda52e65edaa745f4180d8b1e3c4c40
daorejuela1 <daorejuela1@outlook.com>
2023-01-22 23:14:23 -0500

    Updated test files for task 2

--- task_2/test_csv_generator.py
+++ task_2/test_csv_generator.py
@@ -8,6 +8,8 @@ import os
 import csv
 import string
 from csv_generator import DataGenerator
+TEST_FILE_NAME = "test_data.csv"
+TEST_ROWS_QUANTITY = 100
 
 
 class TestStringMethods(unittest.TestCase):
@@ -15,48 +17,44 @@ class TestStringMethods(unittest.TestCase):
         """
         Test if file exists
         """
-        file_name = "test_data.csv"
-        DataGenerator(100, file_name).write_data_to_csv_file()
-        self.assertTrue(os.path.exists(file_name))
-        os.remove(file_name)
+        DataGenerator(TEST_ROWS_QUANTITY, TEST_FILE_NAME).write_data_to_csv_file()
+        self.assertTrue(os.path.exists(TEST_FILE_NAME))
+        os.remove(TEST_FILE_NAME)
 
     def test_file_content(self):
         """
         Test if file contains correct data
         """
-        file_name = "test_data.csv"
-        DataGenerator(100, file_name).write_data_to_csv_file()
-        with open(file_name, "r") as csv_file:
+        DataGenerator(TEST_ROWS_QUANTITY, TEST_FILE_NAME).write_data_to_csv_file()
+        with open(TEST_FILE_NAME, "r") as csv_file:
             reader = csv.reader(csv_file, delimiter=",")
             for row in reader:
                 self.assertTrue(row[0].isdigit())
                 self.assertTrue(len(row[1]) == 10)
-        os.remove(file_name)
+        os.remove(TEST_FILE_NAME)
 
     def test_file_quantity(self):
         """
         Test if file contains correct quantity of rows
         """
-        file_name = "test_data.csv"
-        DataGenerator(100, file_name).write_data_to_csv_file()
-        with open(file_name, "r") as csv_file:
+        DataGenerator(TEST_ROWS_QUANTITY, TEST_FILE_NAME).write_data_to_csv_file()
+        with open(TEST_FILE_NAME, "r") as csv_file:
             reader = csv.reader(csv_file, delimiter=",")
             self.assertEqual(len(list(reader)), 100)
-        os.remove(file_name)
+        os.remove(TEST_FILE_NAME)
 
     def test_file_content_random(self):
         """
         Test if file contains correct data
         """
-        file_name = "test_data.csv"
-        DataGenerator(100, file_name).write_data_to_csv_file()
-        with open(file_name, "r") as csv_file:
+        DataGenerator(TEST_ROWS_QUANTITY, TEST_FILE_NAME).write_data_to_csv_file()
+        with open(TEST_FILE_NAME, "r") as csv_file:
             reader = csv.reader(csv_file, delimiter=",")
             for row in reader:
                 self.assertTrue(int(row[0]) in range(1, 10000))
                 self.assertTrue(len(row[1]) == 10)
                 self.assertTrue(all(c in string.ascii_uppercase + string.digits for c in row[1]))
-        os.remove(file_name)
+        os.remove(TEST_FILE_NAME)
 
 
 if __name__ == "__main__":
